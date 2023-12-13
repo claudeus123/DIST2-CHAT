5cc00166ffd4122985351e1dc463a116a2449960
claudeus123 <claudio.pizarro@alumnos.ucn.cl>
2023-12-13 00:17:47 -0300

    removed cors

--- router/router.go
+++ router/router.go
@@ -13,17 +13,17 @@ var r *gin.Engine
 func InitRouter(wsHandler *ws.Handler) {
 	r = gin.Default()
 
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"GET", "POST"},
-		AllowHeaders:     []string{"Content-Type"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		AllowOriginFunc: func(origin string) bool {
-			return true
-		},
-		MaxAge: 12 * time.Hour,
-	}))
+	// r.Use(cors.New(cors.Config{
+	// 	AllowOrigins:     []string{"*"},
+	// 	AllowMethods:     []string{"GET", "POST"},
+	// 	AllowHeaders:     []string{"Content-Type"},
+	// 	ExposeHeaders:    []string{"Content-Length"},
+	// 	AllowCredentials: true,
+	// 	AllowOriginFunc: func(origin string) bool {
+	// 		return true
+	// 	},
+	// 	MaxAge: 12 * time.Hour,
+	// }))
 
 	// r.Use(middlewares.Validate)
 	r.GET("/", func(c *gin.Context) {
